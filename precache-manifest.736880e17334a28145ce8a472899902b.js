5da5fed4c3435d3390e360b7ff90a66569cf2ed9
DESKTOP-ROPIOGL\Ashwin <winkudva@yahoo.com>
2019-01-15 00:28:00 -0600

    Updates

---
+++ precache-manifest.736880e17334a28145ce8a472899902b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/book/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c6408182fc6691fc3777",
+    "url": "/book/static/js/runtime~main.c6408182.js"
+  },
+  {
+    "revision": "639ab7af75eb4c52bb36",
+    "url": "/book/static/js/main.639ab7af.chunk.js"
+  },
+  {
+    "revision": "baa5d0560b2d7e563917",
+    "url": "/book/static/js/1.baa5d056.chunk.js"
+  },
+  {
+    "revision": "639ab7af75eb4c52bb36",
+    "url": "/book/static/css/main.e3f525aa.chunk.css"
+  },
+  {
+    "revision": "baa5d0560b2d7e563917",
+    "url": "/book/static/css/1.cae55492.chunk.css"
+  },
+  {
+    "revision": "75f5d4c6252f6bda3c2bfcea9d04c876",
+    "url": "/book/index.html"
+  }
+];
